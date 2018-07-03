2c99f752c30a3584144b16d2bd149ea47c68e393
BaroH4er <parshin.ewg@gmail.com>
2018-07-03 20:06:49 +0300

    upgrade markdown

--- test/specs/markdown.js
+++ test/specs/markdown.js
@@ -1,9 +1,11 @@
-var request = require('request');
-var webhook = 'https://hooks.slack.com/services/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';
+//var request = require('request');
+//var webhook = 'https://hooks.slack.com/services/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';
 
 
 describe('Markdown', ()=> {
-request.post('', (err, body) => {
+    
+    
+/*request.post('', (err, body) => {
 request ({
  method: 'POST',
  url: webhook,
@@ -11,36 +13,59 @@ request ({
      "text": "Markdown test start "}
 });
 
-});
-    it('/submit - OK', ()=> {
-        browser.url('https://sandbox.golos.io/submit')
-        browser.getTitle().should.be.equal('Создать пост | GOLOS.io Блоги')
+});*/
+
+
+    it('submit.', ()=> {
+        browser
+            .url('https://sandbox.golos.io/submit')
+            .getTitle().should.be.equal('Создать пост | GOLOS.io Блоги');
+       
+        browser.saveScreenshot('reportsImg/result1.png')
     });
 
+    it('Title', ()=> {
+        browser.setValue('.PostTitle__input', 'AUTO_TEST.')
+        browser.pause(1000);
+        browser.getText('.Hint__content').should.be.equal('Не может заканчиваться на точку')
+        browser.saveScreenshot('reportsImg/result2.png')
+        browser.setValue('.PostTitle__input', 'AUTO_TEST')
+    });
+    it('Body', ()=> {
+
+
+
+    browser.click('.MarkdownEditor')
+    browser.pause(1000)
+    var result = browser.elementActive();
+    var activeElement = result.value && result.value.ELEMENT;
+//        browser.elementIdValue('activeElement', 'world');
+browser.elementIdValue(activeElement, ["ENTER"]);
+
+
+
+        
+        browser.pause(3000)        
+        browser.saveScreenshot('reportsImg/result3.png')
+        
+        
+    }); 
     
-    it('Check links', ()=> {
-        let links = browser.elements('a')
+    
+    it('Footer', ()=> {
 
-        for (let i=0; i < links.value.length; i++) {
-          let link = links.value[i].ELEMENT,
-              href = browser.elementIdAttribute(link,'href'),
-              status = true
-
-          console.log(href.value)
-
-          fetch(href.value)
-            .then( function(res){
-            })
-            .catch( function(err){
-              status = false
-            })
-
-          expect(status).to.be.true;
-        }
-         
+        browser
+            .setValue('.TagInput__input', 'test')
+            .pause(1000)
     });
     
-    it('Report', ()=> {
+    it('Posting', ()=> {
+
+        browser.click('.Button .Button_primary')    
+        
+    });
+    
+ /*   it('Report', ()=> {
 request.post('', (err, body) => {
 request ({
  method: 'POST',
@@ -50,6 +75,55 @@ request ({
 });
 
 });
-});
+});*/
   
 });
+
+
+
+
+
+
+
+
+
+/********************************************************************************/
+
+
+     /*   let links = browser.elements('.MET__new-line-actions')
+
+        for (let i=0; i < links.value.length; i++) {
+          let link = links.value[i].ELEMENT,
+              span = browser.elementIdAttribute(link,'span'),
+              status = true
+          console.log(span.value)
+                
+          fetch(span.value)
+            .then( function(res){
+            })
+            .catch( function(err){
+              status = false
+            })
+          expect(status).to.be.true;
+        }*/
+        
+       // browser.getValue('.Icon editor-toolbar/picture MET__new-line-item MET__new-line-icon')
+       
+       
+       
+       
+       
+       
+       
+       
+          //browser.click('.MarkdownEditor')
+        //browser.setValue('.CodeMirror pre', 'Markdown autotest')
+        //browser.pause(1000)
+        
+       // browser.click('.MarkdownEditor')
+       // browser.ElEMENT('.MET__new-line-actions').click('span*=Добавить изображение')
+        // browser.setValue('#q','webdriver')
+        
+        //var data = new Buffer("Hello World").toString('base64')
+        //browser.pushFile('reportsImg/result2.png', data)
+        //browser.click('.TagInput__input-plus')
